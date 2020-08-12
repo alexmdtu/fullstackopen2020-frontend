de3e02d7130144171e9969c8cb1a46f6b36d22bc
Alexander Tu <alexander.md.tu@gmail.com>
2020-08-12 14:40:35 +0200

    updated code to be the same as from sample solution

--- src/App.js
+++ src/App.js
@@ -1,33 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import Note from './components/Note'
 import Notification from './components/Notification'
+import Footer from './components/Footer'
 import noteService from './services/notes'
 import loginService from './services/login'
-
-const Footer = () => {
-    const footerStyle = {
-        color: 'green',
-        fontStyle: 'italic',
-        fontSize: 16
-    }
-
-    return (
-        <div style={footerStyle}>
-            <br />
-            <em>Note app, Department of Computer Science, University of Helsinki 2020</em>
-        </div>
-    )
-}
 
 const App = () => {
     const [notes, setNotes] = useState([])
     const [newNote, setNewNote] = useState('')
     const [showAll, setShowAll] = useState(true)
-    const [errorMessage, setErrorMessage] = useState('some error happened...')
+    const [errorMessage, setErrorMessage] = useState(null)
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
     const [user, setUser] = useState(null)
-
 
     useEffect(() => {
         noteService
@@ -52,6 +37,7 @@ const App = () => {
             content: newNote,
             date: new Date().toISOString(),
             important: Math.random() > 0.5,
+            id: notes.length + 1,
         }
 
         noteService
@@ -62,11 +48,7 @@ const App = () => {
             })
     }
 
-    const handleNoteChange = (event) => {
-        setNewNote(event.target.value)
-    }
-
-    const toggleImportanceOf = id => {
+    const toggleImportanceOf = (id) => {
         const note = notes.find(n => n.id === id)
         const changedNote = { ...note, important: !note.important }
 
@@ -82,34 +64,34 @@ const App = () => {
                 setTimeout(() => {
                     setErrorMessage(null)
                 }, 5000)
-                setNotes(notes.filter(n => n.id !== id))
             })
     }
-
-    const notesToShow = showAll
-        ? notes
-        : notes.filter(note => note.important)
 
     const handleLogin = async (event) => {
         event.preventDefault()
         try {
             const user = await loginService.login({
-                username, password
+                username, password,
             })
 
             window.localStorage.setItem(
                 'loggedNoteappUser', JSON.stringify(user)
             )
+
             noteService.setToken(user.token)
             setUser(user)
             setUsername('')
             setPassword('')
-        } catch {
-            setErrorMessage('Wrong Credentials')
+        } catch (exception) {
+            setErrorMessage('wrong credentials')
             setTimeout(() => {
                 setErrorMessage(null)
             }, 5000)
         }
+    }
+
+    const handleNoteChange = (event) => {
+        setNewNote(event.target.value)
     }
 
     const loginForm = () => (
@@ -146,16 +128,19 @@ const App = () => {
         </form>
     )
 
+    const notesToShow = showAll
+        ? notes
+        : notes.filter(note => note.important)
+
     return (
         <div>
             <h1>Notes</h1>
-
             <Notification message={errorMessage} />
 
             {user === null ?
                 loginForm() :
                 <div>
-                    <p>{user.name} logged-in</p>
+                    <p>{user.name} logged in</p>
                     {noteForm()}
                 </div>
             }
@@ -174,6 +159,7 @@ const App = () => {
                     />
                 )}
             </ul>
+
             <Footer />
         </div>
     )
