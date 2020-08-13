e3e29f0912626d8d38812cab10224bdbc7b65813
Alexander Tu <alexander.md.tu@gmail.com>
2020-08-13 09:36:56 +0200

    Refactored note form state to component.

--- src/App.js
+++ src/App.js
@@ -10,13 +10,11 @@ import NoteForm from './components/NoteForm'
 
 const App = () => {
     const [notes, setNotes] = useState([])
-    const [newNote, setNewNote] = useState('')
     const [showAll, setShowAll] = useState(true)
     const [errorMessage, setErrorMessage] = useState(null)
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
     const [user, setUser] = useState(null)
-    const [loginVisible, setLoginVisible] = useState(false)
 
     useEffect(() => {
         noteService
@@ -35,20 +33,11 @@ const App = () => {
         }
     }, [])
 
-    const addNote = (event) => {
-        event.preventDefault()
-        const noteObject = {
-            content: newNote,
-            date: new Date().toISOString(),
-            important: Math.random() > 0.5,
-            id: notes.length + 1,
-        }
-
+    const addNote = (noteObject) => {
         noteService
             .create(noteObject)
             .then(returnedNote => {
                 setNotes(notes.concat(returnedNote))
-                setNewNote('')
             })
     }
 
@@ -94,10 +83,6 @@ const App = () => {
         }
     }
 
-    const handleNoteChange = (event) => {
-        setNewNote(event.target.value)
-    }
-
     const loginForm = () => (
         <Togglable buttonLabel='login'>
             <LoginForm
@@ -112,11 +97,7 @@ const App = () => {
 
     const noteForm = () => (
         <Togglable buttonLabel="new note">
-            <NoteForm
-                onSubmit={addNote}
-                value={newNote}
-                handleChange={handleNoteChange}
-            />
+            <NoteForm createNote={addNote} />
         </Togglable>
     )
 
