5f4da91b1f03e78c94fbb66a74af7de3d8e88c22
Alexander Tu <alexander.md.tu@gmail.com>
2020-08-14 09:39:56 +0200

    Added clicking button in test.

--- src/components/Note.test.js
+++ src/components/Note.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render } from '@testing-library/react'
-import { prettyDOM } from '@testing-library/dom'
+import { prettyDOM, fireEvent } from '@testing-library/dom'
 import Note from './Note'
 
 test('renders content', () => {
@@ -31,4 +31,22 @@ test('renders content', () => {
     expect(div).toHaveTextContent(
         'Component testing is done with react-testing-library'
     )
+})
+
+test('clicking the button calls event handler once', () => {
+    const note = {
+        content: 'Component testing is done with react-testing-library',
+        important: true
+    }
+
+    const mockHandler = jest.fn()
+
+    const component = render(
+        <Note note={note} toggleImportance={mockHandler} />
+    )
+
+    const button = component.getByText('make not important')
+    fireEvent.click(button)
+
+    expect(mockHandler.mock.calls).toHaveLength(1)
 })
