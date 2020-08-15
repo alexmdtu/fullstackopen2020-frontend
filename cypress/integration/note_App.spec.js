74e5e2a2937dbc18684f2f04982f62e7f363a986
Alexander Tu <alexander.md.tu@gmail.com>
2020-08-15 13:35:12 +0200

    Added test for login form.

--- cypress/integration/note_App.spec.js
+++ cypress/integration/note_App.spec.js
@@ -1,9 +1,22 @@
 /// <reference types="Cypress" />
 
 describe('Note app', function () {
-    it('front page can be opened', function () {
+    beforeEach(function () {
         cy.visit('http://localhost:3000')
+    })
+
+    it('front page can be opened', function () {
         cy.contains('Notes')
         cy.contains('Note app, Department of Computer Science, University of Helsinki 2020')
+    })
+
+    it('login form can be opened', function () {
+        cy.contains('login').click()
+    })
+
+    it('user can login', function () {
+        cy.contains('login').click()
+        cy.get('input:first').type('mluukkai')
+        cy.get('input:last').type('salainen')
     })
 })
