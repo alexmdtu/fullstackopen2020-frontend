11187f38fce5761f33f82e280508ff6e6f980556
Alexander Tu <alexander.md.tu@gmail.com>
2020-08-15 14:23:10 +0200

    Changed cypress tests to delete databases before doing tests.

--- cypress/integration/note_App.spec.js
+++ cypress/integration/note_App.spec.js
@@ -2,6 +2,13 @@
 
 describe('Note app', function () {
     beforeEach(function () {
+        cy.request('POST', 'http://localhost:3001/api/testing/reset')
+        const user = {
+            name: 'Alex',
+            username: 'atu',
+            password: 'secret'
+        }
+        cy.request('POST', 'http://localhost:3001/api/users', user)
         cy.visit('http://localhost:3000')
     })
 
